af794850498c394425f877b25c1966332c31430b
Nera <mail.for.spam.and.sth@gmail.com>
2025-04-07 20:13:15 +0300

    Resizable Columns

--- main.js
+++ main.js
@@ -1,17 +1,58 @@
 const row = document.getElementById("tableRow");
+const table = document.getElementById('tableId');
 const template = document.getElementById("columnTemplate").content;
 colNum = 0
 
 function addNewColumn() {
-    colNum++;
     const newColumn = template.cloneNode(true);
+    table.style.width = table.offsetWidth + row.children[0].offsetWidth + 'px';
+
     row.appendChild(newColumn);
-    row.lastElementChild.innerHTML = `${colNum} Колонка <div class="resizer"></div>`;
-    console.log(document.getElementById("tableRow"))
+    colNum++;
+
+    row.lastElementChild.children[0].textContent = `${colNum} Колонка`;
+    setColListeners(row.lastElementChild);
 }
 
 function popLastColumn() {
     colNum < 1 ? colNum : colNum--;
-    if (row.children.length > 1) 
-        { row.removeChild(row.lastElementChild); }
+    if (row.children.length > 1) { 
+        table.style.width = table.offsetWidth - row.lastElementChild.offsetWidth - 5 + "px";
+        row.removeChild(row.lastElementChild); 
+    }
+}
+
+function setColListeners(col) {
+    var pageX, curCol, curColWidth, tableWidth;
+    div = col.children[1];
+
+    div.addEventListener('mousedown', function(e) { 
+        tableWidth = table.offsetWidth;
+        curCol = e.target.parentElement;
+        pageX = e.pageX;
+
+        curColWidth = curCol.offsetWidth - 5;
+    });
+
+    document.addEventListener('mousemove', function(e) {
+        if (curCol) {
+            var diffX = e.pageX - pageX;
+            
+            if (curColWidth + diffX >= 150) {
+                curCol.style.width = (curColWidth + diffX) + 'px'; 
+                table.style.width = tableWidth + diffX + "px"
+                
+                // фиксируем размер колонки
+                curCol.style.minWidth = curCol.style.width;
+                curCol.style.maxWidth = curCol.style.width;   
+            }
+        }
+    });
+    
+    document.addEventListener('mouseup', function(e) {
+        pageX = undefined;
+        curCol = undefined;
+        curColWidth = undefined;
+        tableWidth = undefined;
+    });
 }
